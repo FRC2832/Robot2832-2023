3cc328f00e91327bc94dbe0f11e790512f30b8e1
Starpilot785 <starpilot785@gmail.com>
2023-01-20 18:22:04 -0500

    Fixed Variable names

--- src/main/java/frc/robot/interfaces/IDriveControls.java
+++ src/main/java/frc/robot/interfaces/IDriveControls.java
@@ -4,10 +4,10 @@ public interface IDriveControls {
     double GetXDrivePct();
     double GetYDrivePct();
     double GetTurnPct();
-    //double GetAxis1Pct(); //Might change if design changes.
-    //double GetAxis2Pct();
-    //boolean IntakeWheelsRequested();
-    //boolean OuttakeWheelsRequested();
+    //double GetArmAxis1Pct(); //Might change if design changes.
+    //double GetArmAxis2Pct();
+    //boolean IntakeConeRequested();
+    //boolean OuttakeConeRequested();
     //boolean CubeGrabCloseRequested(); 
     //boolean CubeGrabOpenRequested();
     boolean BoostTriggerRequested();
