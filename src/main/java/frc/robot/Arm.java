36e512542a3bb909ecd5b1404bc76f0a68a8ea3d
Brendan Goll <54559443+bgoll1006@users.noreply.github.com>
2023-01-27 21:08:07 -0500

    put arm logic in arm.java framework for new equation with different segment lengths
    Co-authored-by: scarmain <scarmain@users.noreply.github.com>
    Co-authored-by: Starpilot785 <Starpilot785@users.noreply.github.com>
    Co-authored-by: Rock2307 <Rock2307@users.noreply.github.com>
    Co-authored-by: Scott Moore <samoore1@users.noreply.github.com>

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -6,6 +6,8 @@ import frc.robot.interfaces.IArmControl;
 
 public class Arm implements Subsystem{
     private IArmControl hardware;
+    private double shoulderAng;
+    private double elbowAng;
     
     public Arm(IArmControl hardware) {
         this.hardware = hardware;
@@ -28,11 +30,13 @@ public class Arm implements Subsystem{
 
     
     public double getShoulderAngle() {
-        return hardware.getShoulderAngle();
+        shoulderAng = hardware.getShoulderAngle();
+        return shoulderAng;
     }
     
     public double getElbowAngle() {
-        return hardware.getElbowAngle();
+        elbowAng = hardware.getElbowAngle();
+        return elbowAng;
     }
 
     public void setShoulderMotorVolts(double volts) {
@@ -42,4 +46,65 @@ public class Arm implements Subsystem{
     public void setElbowMotorVolts(double volts) {
         hardware.setElbowMotorVolts(volts);
     }
+
+    //this one will become not good once we figure out the equation for an arm with two segments of different lengths
+    public void calcAngles(double x, double z) { //calculate the angles for each part of the arm to get to the point (x, z)
+        double shoulder = 0;
+        double elbow = 0;
+        boolean side = (x > -46); //arm cannot reach outside of 48 inches, we have a grabber on the arm so it checks for <46
+        boolean armLength = Math.sqrt((x*x) + (z*z)) < 60; //total armLength for 34in plus 26in00934
+        if(side && armLength){
+            double l = Math.abs(x);
+            double h = Math.sqrt(l * l + z * z);
+            double phi = Math.toDegrees(Math.atan(z/l));
+            double theta = Math.toDegrees(Math.acos((h/2)/30));
+            if(x >= 0) {
+                shoulder = phi + theta;
+                elbow = (phi - theta);
+            }
+            else{
+                shoulder = 180 - (phi + theta);
+                elbow = 180 - (phi - theta);
+            }
+        }
+        else { //if requested point is outside limited range then set arm angles to what they were before
+            elbow = getElbowAngle();
+            shoulder = getShoulderAngle();
+        }
+        this.setElbowAngle(elbow);
+        this.setShoulderAngle(shoulder);
+    }
+
+    //we will use this one when we figure out the equation for an arm with two different segment lengths
+    public void newCalcAngles(double x, double z) {
+        double shoulderLength = 34.0;
+        double elbowLength = 26.0;
+        double shoulder = 0;
+        double elbow = 0;
+        boolean side = (x > -46); //arm cannot reach outside of 48 inches, we have a grabber on the arm so it checks for <46
+        boolean armLength = Math.sqrt((x*x) + (z*z)) < 60; //total armLength for 34in plus 26in
+        if(side && armLength){
+            double l = Math.abs(x);
+            double h = Math.sqrt(l * l + z * z);
+            double phi = Math.toDegrees(Math.atan(z/l));
+            double theta = Math.toDegrees(Math.acos((h/2)/30));
+            //double q1equation = Math.atan(x/z) - Math.atan((elbowLength*Math.sin(q2)) / (shoulderlength + elbowlength * cos(q2)));
+            //double q2equation = Math.PI - Math.acos()
+            if(x >= 0) {
+                shoulder = phi + theta;
+                elbow = (phi - theta);
+            }
+            else{
+                shoulder = 180 - (phi + theta);
+                elbow = 180 - (phi - theta);
+            }
+        }
+        else { //if requested point is outside limited range then set arm angles to what they were before
+            elbow = getElbowAngle();
+            shoulder = getShoulderAngle();
+        }
+        this.setElbowAngle(elbow);
+        this.setShoulderAngle(shoulder);
+    }
+
 }
