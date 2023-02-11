0cecbc4f1365694053c22991457df0b80217a661
mkhisty <malharkhisty@gmail.com>
2023-02-11 15:15:15 -0500

    Created the LED_CONTROLLER
    Made the thing which controls the LED Display.

---
+++ src/main/java/frc/robot/LED_controller.java
@@ -0,0 +1,55 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.SerialPort;
+
+public class DisplayManager(){
+
+public enum cmds {
+    Arm(1),
+    targetting_computer(2),
+    brightness(3),
+    cone(4),
+    cube(5),
+    kitt(6),
+    lightning(7);
+
+    public final int value;
+    Button(int value) {
+      this.value = value;
+}
+
+
+sp = SerialPort​(int 9600, SerialPort.Port "kOnboard")
+
+public boolean send(cmds prefix,Optional<Integer> num){
+    switch(prefix){
+    case Arm:
+        cmd ="a"
+        break;
+    case targetting_computer: 
+        cmd ="t"
+        break;
+    case brightness:
+        cmd ="b"
+        break;
+    case cone:
+        cmd ="cone"
+        break;
+    case cube: 
+        cmd ="cube"
+        break;
+    case kitt:
+        cmd ="kitt"
+        break;
+    case lightning:
+        cmd ="l"
+        break;
+    }}
+
+    if(num.isPresent() ){
+        sp.writeString(cmd+Integer.toString(num)+"/n");
+    }
+    else{
+    sp.writeString(cmd+"/n");
+    }
+}
+}
