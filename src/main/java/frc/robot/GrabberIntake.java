8b9ff23d9c3aa34d1fa25f743956ea9c9d846b75
Brendan Goll <54559443+bgoll1006@users.noreply.github.com>
2023-02-11 13:28:43 -0500

    Cleaned up Grab logic
    Co-authored-by: Aryan3221 <Aryan3221@users.noreply.github.com>

--- src/main/java/frc/robot/GrabberIntake.java
+++ src/main/java/frc/robot/GrabberIntake.java
@@ -1,4 +1,5 @@
 package frc.robot;
+
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj.Timer;
 import com.revrobotics.CANSparkMax;
@@ -10,39 +11,53 @@ public class GrabberIntake implements Subsystem {
     private CANSparkMax intakeMotor;
     private double nomVolt;
     private Timer timer;
+    private boolean done;
 
     public GrabberIntake(){
-        //grabber = new DoubleSolenoid(PneumaticsModuleType.REVPH,0,1);
+        
         intakeMotor = new CANSparkMax(48,MotorType.kBrushless);
         intakeMotor.setInverted(true);
         timer = new Timer();
-        
+        done = false;
     }
     public void periodic() {
         SmartDashboard.putNumber("Intake Speed%", intakeMotor.get());
 
     }
 
-    public void Grab(boolean grabbing) {
-        if (grabbing) {
+    public void Grab(boolean forward) {
+        if(forward && done) {
+            intakeOff();
+            return;
+        }
+        else {
+            done = false;
+        }
+
+        if (forward) {
             intakeMotor.setVoltage(Constants.IntakeVoltage);
         } else {
             intakeMotor.setVoltage(-1 * Constants.IntakeVoltage);
         }
+        
         nomVolt = intakeMotor.getVoltageCompensationNominalVoltage();
         SmartDashboard.putNumber("Intake Nominal Voltage", nomVolt);
         
         if(nomVolt >= 0) {
             timer.start();
             if(timer.hasElapsed(2.0)) {
-                intakeMotor.setVoltage(0.0);
+                intakeOff();
+                done = true;
             }
         }
         else {
             timer.stop();
             timer.reset();
         }
-        
-        
     }
+
+    public void intakeOff(){
+        intakeMotor.setVoltage(0.0);
+    }
+
 }
