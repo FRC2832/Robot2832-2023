aa3faec0da0294420f85b7d5c53a09ba7d970bee
Brendan Goll <54559443+bgoll1006@users.noreply.github.com>
2023-02-15 21:28:56 -0500

    implemented sequential commands

--- src/main/java/frc/robot/commands/InitAutonScore.java
+++ src/main/java/frc/robot/commands/InitAutonScore.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Arm;
 import frc.robot.Constants;
 import frc.robot.GrabberIntake;
@@ -26,19 +27,16 @@ public class InitAutonScore extends CommandBase {
 
     @Override
     public void execute() {
-        if(this.location == 0) { //Score to top
-            new ArmAutonPoint(this.arm, Constants.ArmToScoreTop_X, Constants.ArmToScoreTop_Z);
-        } else if(this.location == 1) { //Score to middle
-            new ArmAutonPoint(this.arm, Constants.ArmToScoreMiddle_X, Constants.ArmToScoreMiddle_Z);
-        } else { //Score to low
-            new ArmAutonPoint(this.arm, Constants.ArmToScoreLow_X, Constants.ArmToScoreLow_Z);
-        }
-
+         
+        new SequentialCommandGroup(
+        //Score to top
+        new ArmAutonPoint(this.arm, Constants.ArmToScoreTop_X, Constants.ArmToScoreTop_Z),
         //Release cone
-        intake.Grab(false); 
-
+        new IntakeBackward(this.intake),
         //Reset arm position
-        new ArmAutonPoint(this.arm, Constants.ArmToSecureLocation_X, Constants.ArmToSecureLocation_Z);
+        new ArmAutonPoint(this.arm, Constants.ArmToSecureLocation_X, Constants.ArmToSecureLocation_Z)
+        );
+
     }
 
     @Override
