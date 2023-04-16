e957161cd36ebc9c1dad077a6635562c160a0762
scarmain <scarmain@gmail.com>
2023-04-16 12:22:40 -0400

    more tuning

--- src/main/java/frc/robot/AutonChooser.java
+++ src/main/java/frc/robot/AutonChooser.java
@@ -60,6 +60,8 @@ public class AutonChooser {
     private static final String kPlan3P = "3 piece pathplan";
 
     private String AutonomousStartPosition;
+    ArrayList<PathPlannerTrajectory> threePieceLow;
+    SwerveAutoBuilder autoBuilder;
 
     public AutonChooser(ISwerveDrive drive, Odometry odometry, Intake intake, Arm arm, Pivot pivot){
         this.drive = drive;
@@ -86,6 +88,34 @@ public class AutonChooser {
         SmartDashboard.putData("StartPos Select", startPosChooser);
 
         setStartPos();
+
+        // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
+        // for every path in the group
+        threePieceLow = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("3PieceLow", new PathConstraints(3, 3));
+
+        // This is just an example event map. It would be better to have a constant, global event map
+        // in your code that will be used by all path following commands.
+        HashMap<String, Command> eventMap = new HashMap<>();
+        eventMap.put("armDown", new ArmAutonPoint(arm, Constants.ArmToPickupGround_X, Constants.ArmToPickupGround_Z-5));
+        eventMap.put("armScoop", new ArmAutonPoint(arm, Constants.ArmToPickupGround_X+4, Constants.ArmToPickupGround_Z-5));
+        eventMap.put("armScore", new ArmAutonPoint(arm, Constants.ArmToPickupGroundBack_X, Constants.ArmToPickupGroundBack_Z));
+        eventMap.put("spit", spit());
+        eventMap.put("suck", suck().withTimeout(1));
+        eventMap.put("pivotPickup", new PivotSetPoint(pivot, 60));
+        //eventMap.put("intakeDown", new IntakeDown());
+
+        // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
+        autoBuilder = new SwerveAutoBuilder(
+            odometry::getPose, // Pose2d supplier
+            this::setStartPos, // Pose2d consumer, used to reset odometry at the beginning of auto
+            drive.getKinematics(), // SwerveDriveKinematics
+            new PIDConstants(6, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
+            new PIDConstants(6, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+            drive::setWheelCommand, // Module states consumer used to output to the drive subsystem
+            eventMap,
+            false, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+            drive // The drive subsystem. Used to properly set the requirements of path following commands
+        );
     }
 
     public Command getAuton(){
@@ -366,46 +396,11 @@ public class AutonChooser {
     }
 
     public Command autoPlanThreePiece() {
-        // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-        // for every path in the group
-        ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("3PieceLow", new PathConstraints(2, 3));
-
-        // This is just an example event map. It would be better to have a constant, global event map
-        // in your code that will be used by all path following commands.
-        HashMap<String, Command> eventMap = new HashMap<>();
-        eventMap.put("armDown", new ArmAutonPoint(arm, Constants.ArmToPickupGround_X, Constants.ArmToPickupGround_Z-5));
-        eventMap.put("armScoop", new ArmAutonPoint(arm, Constants.ArmToPickupGround_X-2, Constants.ArmToPickupGround_Z-5));
-        eventMap.put("armScore", new ArmAutonPoint(arm, Constants.ArmToPickupGroundBack_X, Constants.ArmToPickupGroundBack_Z));
-        eventMap.put("spit", spit());
-        eventMap.put("suck", suck());
-        eventMap.put("pivotPickup", new PivotSetPoint(pivot, 80));
-        //eventMap.put("intakeDown", new IntakeDown());
-
-        // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
-        SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
-            odometry::getPose, // Pose2d supplier
-            this::setStartPos, // Pose2d consumer, used to reset odometry at the beginning of auto
-            drive.getKinematics(), // SwerveDriveKinematics
-            new PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-            new PIDConstants(2.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
-            drive::setWheelCommand, // Module states consumer used to output to the drive subsystem
-            eventMap,
-            true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-            drive // The drive subsystem. Used to properly set the requirements of path following commands
-        );
-
-        return autoBuilder.fullAuto(pathGroup);
+        return autoBuilder.fullAuto(threePieceLow);
     }
 
     public void setStartPos(Pose2d startPos) {
-        if(DriverStation.getAlliance() == Alliance.Red) {
-            //flip the orientation because Path Planner thinks the bottom left corner is always the starting position, but won't flip the field2d object
-            Pose2d newPos = new Pose2d(15.98-startPos.getX(), 8.21-startPos.getY(),startPos.getRotation().rotateBy(Rotation2d.fromDegrees(180)));
-            startPosition = newPos;
-        } else {
-            startPosition = startPos;
-        }
-        odometry.resetPose(startPosition);
+        odometry.resetPose(startPos);
     }
 
     //START POSITION SETTER (FROM SHUFFLEBOARD INPUT)    
